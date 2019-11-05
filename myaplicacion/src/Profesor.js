2cfbea7825e54c7db4cba1c7bd3c2830ae93585f
ponsger <german.aopg@gmail.com>
2019-11-04 22:25:21 -0600

    Borrado lo de react

--- myaplicacion/src/Profesor.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Bienvenida from './Bienvenida';
-import './css/index.css'
-
-ReactDOM.render(<Bienvenida boton="Ver mi informacion"/>,document.getElementById('bienvenido'));
