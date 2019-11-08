bf3ee958f251645d0f50a9ea40f9ceaccb91ca2d
Rodrigo Salazar <mc.sr.rod@gmail.com>
2019-11-07 22:08:33 -0600

    Clase respuesta

---
+++ ProjectFinalStruts/src/java/xml/Respuesta.java
@@ -0,0 +1,58 @@
+package xml;
+
+/**
+ *
+ * @author RodrigoSalazar
+ */
+public class Respuesta {
+    int numeroExamen;
+    int numeroEjercicio;
+    int numeroPregunta;
+    String respuestaEjercicio;
+    String respuestaPregunta;
+
+    public Respuesta() {
+    }
+
+    public int getNumeroExamen() {
+        return numeroExamen;
+    }
+
+    public void setNumeroExamen(int numeroExamen) {
+        this.numeroExamen = numeroExamen;
+    }
+
+    public int getNumeroEjercicio() {
+        return numeroEjercicio;
+    }
+
+    public void setNumeroEjercicio(int numeroEjercicio) {
+        this.numeroEjercicio = numeroEjercicio;
+    }
+
+    public int getNumeroPregunta() {
+        return numeroPregunta;
+    }
+
+    public void setNumeroPregunta(int numeroPregunta) {
+        this.numeroPregunta = numeroPregunta;
+    }
+
+    public String getRespuestaEjercicio() {
+        return respuestaEjercicio;
+    }
+
+    public void setRespuestaEjercicio(String respuestaEjercicio) {
+        this.respuestaEjercicio = respuestaEjercicio;
+    }
+
+    public String getRespuestaPregunta() {
+        return respuestaPregunta;
+    }
+
+    public void setRespuestaPregunta(String respuestaPregunta) {
+        this.respuestaPregunta = respuestaPregunta;
+    }
+    
+    
+}
