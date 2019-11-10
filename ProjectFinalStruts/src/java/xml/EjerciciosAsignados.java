e53154c0d1c3b52f510b97ea2b884acb704f7203
Rodrigo Salazar <mc.sr.rod@gmail.com>
2019-11-09 18:39:43 -0600

    Asignados

---
+++ ProjectFinalStruts/src/java/xml/EjerciciosAsignados.java
@@ -0,0 +1,38 @@
+package xml;
+
+/**
+ *
+ * @author RodrigoSalazar
+ */
+public class EjerciciosAsignados {
+    int numeroEjercicio;
+    String grupoCompleto;
+    int numeroAlumno;
+
+    public EjerciciosAsignados() {
+    }
+
+    public int getNumeroEjercicio() {
+        return numeroEjercicio;
+    }
+
+    public void setNumeroEjercicio(int numeroEjercicio) {
+        this.numeroEjercicio = numeroEjercicio;
+    }
+
+    public String getGrupoCompleto() {
+        return grupoCompleto;
+    }
+
+    public void setGrupoCompleto(String grupoCompleto) {
+        this.grupoCompleto = grupoCompleto;
+    }
+
+    public int getNumeroAlumno() {
+        return numeroAlumno;
+    }
+
+    public void setNumeroAlumno(int numeroAlumno) {
+        this.numeroAlumno = numeroAlumno;
+    }
+}
---
+++ ProjectFinalStruts/src/java/xml/ExamenesAsignados.java
@@ -0,0 +1,38 @@
+package xml;
+
+/**
+ *
+ * @author RodrigoSalazar
+ */
+public class ExamenesAsignados {
+    int numeroExamen;
+    String grupoCompleto;
+    int numeroAlumno;
+
+    public ExamenesAsignados() {
+    }
+
+    public int getNumeroExamen() {
+        return numeroExamen;
+    }
+
+    public void setNumeroExamen(int numeroExamen) {
+        this.numeroExamen = numeroExamen;
+    }
+
+    public String getGrupoCompleto() {
+        return grupoCompleto;
+    }
+
+    public void setGrupoCompleto(String grupoCompleto) {
+        this.grupoCompleto = grupoCompleto;
+    }
+
+    public int getNumeroAlumno() {
+        return numeroAlumno;
+    }
+
+    public void setNumeroAlumno(int numeroAlumno) {
+        this.numeroAlumno = numeroAlumno;
+    }
+}
