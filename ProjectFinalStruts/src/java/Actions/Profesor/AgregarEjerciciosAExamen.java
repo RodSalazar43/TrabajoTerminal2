092753ce17944898230985612ad44eda9f851ee0
Rodrigo Salazar <mc.sr.rod@gmail.com>
2019-11-10 18:00:20 -0600

    Update clase agregar ejercicios a examen

--- ProjectFinalStruts/src/java/Actions/Profesor/AgregarEjerciciosAExamen.java
+++ ProjectFinalStruts/src/java/Actions/Profesor/AgregarEjerciciosAExamen.java
@@ -30,6 +30,42 @@ public class AgregarEjerciciosAExamen {
     public String execute(){
         Session hibernateSession;
         hibernateSession = HibernateUtil.getSessionFactory().openSession();
+        
+        /*
+        XMLActions xml = new XMLActions();
+        
+        Grupo grupo = (Grupo)hibernateSession.load(Grupo.class, this.idGrupo);
+        String ruta = grupo.getRutaXmlasignados();
+        
+        List examenes = xml.cargarXmlExamenesAsignados(ruta);
+        List ejercicios = xml.cargarXmlEjerciciosAsignados(ruta);
+        List preguntas = xml.cargarXmlPreguntasAsignadas(ruta);
+        
+        ArrayList<ExamenesAsignados> datosExamenes = xml.convierte2ArrayListExamenesAsignados(examenes);
+        ArrayList<EjerciciosAsignados> datosEjercicios = xml.convierte2ArrayListEjerciciosAsignados(ejercicios);
+        ArrayList<PreguntasAsignadas> datosPreguntas = xml.convierte2ArrayListPreguntasAsignadas(preguntas);
+        
+        String cachos[] = this.numerosAlumnos.split(",");
+        
+        for (String cacho : cachos) {
+            EjerciciosAsignados ea = new EjerciciosAsignados();
+            
+            ea.setGrupoCompleto("no");
+            ea.setIdGrupo(this.idGrupo);
+            ea.setNumeroAlumno(Integer.parseInt(cacho));
+            ea.setNumeroEjercicio(this.numeroEjercicio);
+            
+            datosEjercicios.add(ea);
+        }
+        
+        if(xml.guardarXmlAsignados(datosExamenes, datosEjercicios, datosPreguntas, ruta)){
+            return SUCCESS;
+        }
+        else{
+            return ERROR;
+        }
+        */
+              
         return "";
     }
 }
