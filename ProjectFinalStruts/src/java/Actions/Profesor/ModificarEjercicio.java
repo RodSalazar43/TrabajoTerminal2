907fc4555da13f8296db218ef0d0d852e9c3794c
Rodrigo Salazar <mc.sr.rod@gmail.com>
2019-11-12 05:24:30 -0600

    Modificar ejercicio completo, creo

--- ProjectFinalStruts/src/java/Actions/Profesor/ModificarEjercicio.java
+++ ProjectFinalStruts/src/java/Actions/Profesor/ModificarEjercicio.java
@@ -6,23 +6,49 @@ import java.util.ArrayList;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import xml.Pregunta;
 import xml.XMLActions;
 import static Complementos.Operaciones.*;
+import xml.Ejercicio;
 
 /**
  *
  * @author RodrigoSalazar
  */
-public class ModificarPregunta {
+public class ModificarEjercicio {
     private int idProfesor;
     private String numero;
     private String indicaciones;
-    private String respuesta;
     private String nombre;
     private String opcion1;
     private String opcion2;
+    private String opcion3;
+    private String opcion4;
+    private String resultado;
 
+    public String getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(String resultado) {
+        this.resultado = resultado;
+    }
+
+    public String getOpcion3() {
+        return opcion3;
+    }
+
+    public void setOpcion3(String opcion3) {
+        this.opcion3 = opcion3;
+    }
+
+    public String getOpcion4() {
+        return opcion4;
+    }
+
+    public void setOpcion4(String opcion4) {
+        this.opcion4 = opcion4;
+    }
+    
     public int getIdProfesor() {
         return idProfesor;
     }
@@ -45,14 +71,6 @@ public class ModificarPregunta {
 
     public void setIndicaciones(String indicaciones) {
         this.indicaciones = indicaciones;
-    }
-
-    public String getRespuesta() {
-        return respuesta;
-    }
-
-    public void setRespuesta(String respuesta) {
-        this.respuesta = respuesta;
     }
 
     public String getNombre() {
@@ -88,22 +106,25 @@ public class ModificarPregunta {
         String ruta = profesor.getRutaXmlpreguntas();
         
         XMLActions xml = new XMLActions();        
-        List lista = xml.cargarXmlPreguntas(ruta);
-        ArrayList<Pregunta> preguntas = xml.convierte2ArrayListPreguntas(lista);
-        Pregunta original = preguntas.get(Integer.parseInt(this.numero) - 1);
-        Pregunta preguntaModificada = new Pregunta();
+        List lista = xml.cargarXmlEjercicios(ruta);
+        ArrayList<Ejercicio> ejercicios = xml.convierte2ArrayListEjercicios(lista);
+        Ejercicio original = ejercicios.get(Integer.parseInt(this.numero) - 1);
         
-        preguntaModificada.setNombre(nombre);
-        preguntaModificada.setIndicaciones(indicaciones);
-        preguntaModificada.setNumero(numero);
-        preguntaModificada.setOpcion1(opcion1);
-        preguntaModificada.setOpcion2(opcion2);
-        preguntaModificada.setRespuesta(respuesta);
-        preguntaModificada.setTipo(original.getTipo());
+        Ejercicio ejercicioModificado = new Ejercicio();
         
-        preguntas = xml.modificaPregunta(preguntas, numero, preguntaModificada);
+        ejercicioModificado.setNombre(nombre);
+        ejercicioModificado.setPregunta(indicaciones);
+        ejercicioModificado.setNumero(numero);
+        ejercicioModificado.setOpcion1(opcion1);
+        ejercicioModificado.setOpcion2(opcion2);
+        ejercicioModificado.setOpcion3(opcion3);
+        ejercicioModificado.setOpcion4(opcion4);
+        ejercicioModificado.setResultado(resultado);
+        ejercicioModificado.setTipo(original.getTipo());
         
-        if(xml.guardarXmlPregunta(preguntas, ruta)==true){
+        ejercicios = xml.modificaEjercicio(ejercicios, numero, ejercicioModificado);
+        
+        if(xml.guardarXmlEjercicio(ejercicios, ruta) == true){
             return SUCCESS;
         }else{
             return ERROR;
