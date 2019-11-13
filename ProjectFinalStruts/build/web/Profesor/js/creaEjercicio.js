e0942277e87c6cf455d4e131a9826d11063bd426
ponsger <49313446+ponsger@users.noreply.github.com>
2019-11-13 09:54:55 -0600

    agrega ejercicio

--- ProjectFinalStruts/build/web/Profesor/js/creaEjercicio.js
+++ ProjectFinalStruts/build/web/Profesor/js/creaEjercicio.js
@@ -4,7 +4,61 @@ function creaEjercicio(){
     console.log(muestra.value)
     var formulario="";
     if(muestra.value==="Linea"){
-        formulario+="<div id='formulario' ></div>"
+        formulario+="<div class='contenido-grande alert-secondary'>"
+        formulario+="<div class='card-header bg-dark'>"
+        formulario+="<h3 class='text-center text-white'>Ingresa los datos del nuevo ejercicio a agregar</h3>"
+        formulario+="</div>"
+        formulario+="<div class='card-body'>"
+        formulario+="<form onsubmit='generaEjercicio()'>"
+        formulario+="<label class='font-weight-bold'>Nombre del ejercicio:</label>"
+        formulario+="<input class='form-control border-dark' type='text' name='nombre' value=''/ required>"
+        formulario+="<label class='font-weight-bold'>Ingresa las instrucciones para el alumno:</label>"
+        formulario+="<input class='form-control border-dark' type='nombre' name='instruccion' value='' required/>"
+        formulario+="<label class='font-italic font-weight-bold text-uppercase'>Ingresa las opciones para que el alumno elija</label><br>"
+        formulario+="<center>"
+        formulario+="<div class='row'>"
+        formulario+="<label class='font-weight-bold col-3'>"
+        formulario+="Opcion 1:"
+        formulario+="<input class='form-control border-dark' type='text' name='opcion1' value='' required/>"
+        formulario+="</label>"
+        formulario+="<label class='font-weight-bold col-3'>"
+        formulario+="Opcion 2:"
+        formulario+="<input class='form-control border-dark' type='text' name='opcion2' value='' required/>"
+        formulario+="</label>"
+        formulario+="<label class='font-weight-bold col-3'>"
+        formulario+="Opcion 3:"
+        formulario+="<input class='form-control border-dark' type='text' name='opcion3' value='' required/>"
+        formulario+="</label>"
+        formulario+="<label class='font-weight-bold col-3'>"
+        formulario+="Opcion 4:"
+        formulario+="<input class='form-control border-dark' type='text' name='opcion4' value='' required/>"
+        formulario+="</label><br>"
+        formulario+="</div>"
+        formulario+="<div class='row'>"
+        formulario+="<label class='font-weight-bold col-3'>"
+        formulario+="Respuesta correcta para opción 1:"
+        formulario+="<input class='form-control border-dark' type='text' name='opcion5' value='' required/>"
+        formulario+="</label>"
+        formulario+="<label class='font-weight-bold col-3'>"
+        formulario+="Respuesta correcta para opción 2:"
+        formulario+="<input class='form-control border-dark' type='text' name='opcion6' value='' required/>"
+        formulario+="</label>"
+        formulario+="<label class='font-weight-bold col-3'>"
+        formulario+="Respuesta correcta para opción 3:"
+        formulario+="<input class='form-control border-dark' type='text' name='opcion7' value='' required/>"
+        formulario+="</label>"
+        formulario+="<label class='font-weight-bold col-3'>"
+        formulario+="Respuesta correcta para opción 4:"
+        formulario+="<input class='form-control border-dark' type='text' name='opcion8' value='' required/>"
+        formulario+="</label><br>"
+        formulario+="</div>"
+        formulario+="</center>"
+        formulario+="<br>"
+        formulario+="<button class='btn btn-success form-control' onclick='generaEjercicio()'>Guardar</button>"
+        formulario+="<br><br>"
+        formulario+="</div>"
+        formulario+="</form> </div>"
+        tipo=muestra.value;
     }else if(muestra.value==="DAD"){
         formulario+="<div class='contenido-grande alert-secondary'>"
         formulario+="<div class='card-header'>"
@@ -50,23 +104,46 @@ function creaEjercicio(){
 }
 
 function generaEjercicio(){
-    var nombre=document.getElementsByName("nombre");
-    var instruccion=document.getElementsByName("instruccion");
-    var opcion1=document.getElementsByName("opcion1")
-    var opcion2=document.getElementsByName("opcion2")
-    var opcion3=document.getElementsByName("opcion3")
-    var opcion4=document.getElementsByName("opcion4")
-    var resultado=document.getElementsByName("resultado")
-    console.log(nombre[0].value)
     var id=getParameterByName("id");
-    if(nombre[0].value===""||instruccion[0].value===""||opcion1[0].value===""||opcion2[0].value===""||opcion3[0].value===""||opcion4[0].value===""||resultado[0].value===""){
+    var determinado="default";
+    if(tipo==="DAD"){
+        var nombre=document.getElementsByName("nombre");
+        var instruccion=document.getElementsByName("instruccion");
+        var opcion1=document.getElementsByName("opcion1")
+        var opcion2=document.getElementsByName("opcion2")
+        var opcion3=document.getElementsByName("opcion3")
+        var opcion4=document.getElementsByName("opcion4")
+        var resultado=document.getElementsByName("resultado")
+     if(nombre[0].value===""||instruccion[0].value===""||opcion1[0].value===""||opcion2[0].value===""||opcion3[0].value===""||opcion4[0].value===""||resultado[0].value===""){
         alert("Por favor llene el campo vacío");
-    }else{
-    let url = 'http://localhost:8080/ProjectFinalStruts/inEjercicio?idProfesor='+id+'&nombre='+nombre[0].value+'&inst='+instruccion[0].value+'&op1='+opcion1[0].value+'&op2='+opcion2[0].value+'&op3='+opcion3[0].value+'&op4='+opcion4[0].value+'&res='+resultado[0].value+'&tipo='+tipo;
+     }else{
+    let url = 'http://localhost:8080/ProjectFinalStruts/AgregarEjercicio?idProfesor='+id+'&nombre='+nombre[0].value+'&inst='+instruccion[0].value+'&op1='+opcion1[0].value+'&op2='+opcion2[0].value+'&op3='+opcion3[0].value+'&op4='+opcion4[0].value+'&op5='+opcion5[0].value+'&op6='+opcion6[0].value+'&op7='+opcion7[0].value+'&op8='+opcion8[0].value+'&res='+resultado[0].value+'&tipo='+tipo;
         //alert('URL:' + url);
         fetch(url).then(response => response.text()).then(data => {
             alert(data);    
         });
+    }   
+     
+    }else if(tipo==="Linea"){
+        var nombre=document.getElementsByName("nombre");
+        var instruccion=document.getElementsByName("instruccion");
+        var opcion1=document.getElementsByName("opcion1")
+        var opcion2=document.getElementsByName("opcion2")
+        var opcion3=document.getElementsByName("opcion3")
+        var opcion4=document.getElementsByName("opcion4")
+        var opcion5=document.getElementsByName("opcion5")
+        var opcion6=document.getElementsByName("opcion6")
+        var opcion7=document.getElementsByName("opcion7")
+        var opcion8=document.getElementsByName("opcion8")
+        if(nombre[0].value===""||instruccion[0].value===""||opcion1[0].value===""||opcion2[0].value===""||opcion3[0].value===""||opcion4[0].value===""||resultado[0].value===""){
+            alert("Por favor llene el campo vacío");
+        }else{
+            let url = 'http://localhost:8080/ProjectFinalStruts/AgregarEjercicio?idProfesor='+id+'&nombre='+nombre[0].value+'&inst='+instruccion[0].value+'&op1='+opcion1[0].value+'&op2='+opcion2[0].value+'&op3='+opcion3[0].value+'&op4='+opcion4[0].value+'&op5='+determinado+'&op6='+determinado+'&op7='+determinado+'&op8='+determinado+'&res='+determinado+'&tipo='+tipo;
+        //alert('URL:' + url);
+            fetch(url).then(response => response.text()).then(data => {
+                alert(data);    
+            });
+        }
     }
     
 }
