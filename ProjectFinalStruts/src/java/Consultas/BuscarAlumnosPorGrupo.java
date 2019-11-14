f9a1599305e7435d78dfb7b237dd00267aa90995
Rodrigo Salazar <mc.sr.rod@gmail.com>
2019-11-14 04:01:03 -0600

    JSON requeridos

---
+++ ProjectFinalStruts/src/java/Consultas/BuscarAlumnosPorGrupo.java
@@ -0,0 +1,80 @@
+package Consultas;
+
+import entitys.HibernateUtil;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.json.simple.JSONObject;
+import static Complementos.Operaciones.*;
+import entitys.Alumno;
+import entitys.Grupo;
+import entitys.Usuario;
+import java.util.Iterator;
+import org.apache.struts2.ServletActionContext;
+import org.hibernate.Query;
+
+/**
+ *
+ * @author RodrigoSalazar
+ */
+public class BuscarAlumnosPorGrupo {
+    private int idGrupo;
+
+    public int getIdGrupo() {
+        return idGrupo;
+    }
+
+    public void setIdGrupo(int idGrupo) {
+        this.idGrupo = idGrupo;
+    }
+
+    public String execute(){
+        Session hibernateSession;
+        hibernateSession = HibernateUtil.getSessionFactory().openSession(); 
+        Transaction t = hibernateSession.beginTransaction();
+        
+        String hql = "FROM Alumno";
+        Query query = hibernateSession.createQuery(hql);
+        Iterator results = query.iterate();
+        
+        JSONObject raiz = new JSONObject();
+        JSONObject obj = new JSONObject();
+        
+        int contador = 0;
+        
+        while(results.hasNext()){
+            Alumno alumnoActual = (Alumno)results.next();
+            
+            if(this.idGrupo == alumnoActual.getGrupo().getIdGrupo()){
+                Usuario usuario = (Usuario)hibernateSession.load(Usuario.class, alumnoActual.getIdUsuario());
+                
+                JSONObject innerObj = new JSONObject();
+                
+                innerObj.put("nombre", usuario.getNombre());
+                innerObj.put("Ap_Paterno", usuario.getApPaterno());
+                innerObj.put("Ap_Materno", usuario.getApMat());
+                
+                obj.put(contador, innerObj);
+                raiz.put("idAlumno",obj);
+                contador++;
+            }
+        }
+        
+        try{
+            String hola=ServletActionContext.getServletContext().getRealPath("/json");
+            System.out.println("***************************************************************");
+            System.out.println(hola);
+           
+            FileWriter file = new FileWriter(ServletActionContext.getServletContext().getRealPath("/json/resultadoAlumnosPorGrupo.json"));
+            file.write(raiz.toJSONString());
+            file.flush();
+            file.close();
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return SUCCESS;
+    }
+}
---
+++ ProjectFinalStruts/src/java/Consultas/BuscarGruposPorProfesor.java
@@ -0,0 +1,73 @@
+package Consultas;
+
+import entitys.HibernateUtil;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.json.simple.JSONObject;
+import static Complementos.Operaciones.*;
+import entitys.Grupo;
+import java.util.Iterator;
+import org.apache.struts2.ServletActionContext;
+import org.hibernate.Query;
+
+/**
+ *
+ * @author RodrigoSalazar
+ */
+public class BuscarGruposPorProfesor {
+    private int idProfesor;
+
+    public int getIdProfesor() {
+        return idProfesor;
+    }
+
+    public void setIdProfesor(int idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+    public String execute(){
+        Session hibernateSession;
+        hibernateSession = HibernateUtil.getSessionFactory().openSession(); 
+        Transaction t = hibernateSession.beginTransaction();
+        
+        String hql = "FROM Grupo";
+        Query query = hibernateSession.createQuery(hql);
+        Iterator results = query.iterate();
+        
+        JSONObject raiz = new JSONObject();
+        JSONObject obj = new JSONObject();
+        
+        int contador = 0;
+        
+        while(results.hasNext()){
+            Grupo grupoActual = (Grupo)results.next();
+            
+            if(this.idProfesor == grupoActual.getProfesor().getIdUsuario()){
+                JSONObject innerObj = new JSONObject();
+                innerObj.put("nombre", grupoActual.getNombre());
+                innerObj.put("turno", grupoActual.getTurno());
+                
+                obj.put(contador, innerObj);
+                raiz.put("idGrupo",obj);
+                contador++;
+            }
+        }
+        
+        try{
+            String hola=ServletActionContext.getServletContext().getRealPath("/json");
+            System.out.println("***************************************************************");
+            System.out.println(hola);
+           
+            FileWriter file = new FileWriter(ServletActionContext.getServletContext().getRealPath("/json/resultadoConsultaGruposPorProfesor.json"));
+            file.write(raiz.toJSONString());
+            file.flush();
+            file.close();
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return SUCCESS;
+    }
+}
