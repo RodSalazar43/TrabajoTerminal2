dc4945a121a9e9bab60137ad3d32b8032c301804
ponsger <german.aopg@gmail.com>
2019-11-13 19:10:21 -0600

    Ver ejercicios y examen al 100 desde el front

--- ProjectFinalStruts/web/Profesor/js/muestraEjercicio.js
+++ ProjectFinalStruts/web/Profesor/js/muestraEjercicio.js
@@ -54,3 +54,10 @@ function cargarTabla() {
       }
         
     }
+    
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
